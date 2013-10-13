9508c58125a96e59f9280532f27e80a3825f7a54
almazhou <zx_new@126.com>
2013-10-13 16:08:54 +0800

    add functionality of substract and multiply

--- src/main/java/MyLength.java
+++ src/main/java/MyLength.java
@@ -16,7 +16,7 @@ public class MyLength {
     }
 
     public MyLength add(MyLength addedLength) {
-        return new MyLength(convertUnitToM(this) + convertUnitToM(addedLength), unit);
+        return new MyLength(convertUnitToM(this) + convertUnitToM(addedLength), "m");
     }
 
     private double convertUnitToM(MyLength length) {
@@ -29,5 +29,37 @@ public class MyLength {
 
     public String getUnit() {
         return unit;
+    }
+
+    public int compareTo(MyLength otherLength) {
+        return (int) (convertUnitToM(this) - convertUnitToM(otherLength));
+    }
+
+    public MyLength subtract(MyLength otherLength) {
+        return new MyLength(convertUnitToM(this) - convertUnitToM(otherLength),"m");
+    }
+
+    public double multiply(MyLength otherLength) {
+        return convertUnitToM(this) * convertUnitToM(otherLength);
+    }
+
+    public double divide(MyLength otherLength) {
+        if(otherLength.getValue()!=0){
+        return convertUnitToM(this)/convertUnitToM(otherLength);
+        }else {
+            throw new RuntimeException("divided length is 0");
+        }
+    }
+
+    public double convertToCm() {
+        return convertUnitToM(this)*100;
+    }
+
+    public double convertToMm() {
+        return convertUnitToM(this)*1000;
+    }
+
+    public double convertToM() {
+        return convertUnitToM(this)*1;
     }
 }
--- src/test/java/LengthTest.java
+++ src/test/java/LengthTest.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class LengthTest {
     @Test
@@ -10,8 +12,8 @@ public class LengthTest {
         MyLength length2 = new MyLength(1, "m");
 
         MyLength result = length1.add(length2);
-        assertThat(result.getValue(),is(2.0));
-        assertThat(result.getUnit(),is("m"));
+        assertThat(result.getValue(), is(2.0));
+        assertThat(result.getUnit(), is("m"));
     }
 
     @Test
@@ -20,8 +22,120 @@ public class LengthTest {
         MyLength length2 = new MyLength(100, "cm");
 
         MyLength result = length1.add(length2);
-        assertThat(result.getValue(),is(2.0));
-        assertThat(result.getUnit(),is("m"));
+        assertThat(result.getValue(), is(2.0));
+        assertThat(result.getUnit(), is("m"));
 
+    }
+
+    @Test
+    public void should_add_get_m_for_unit() throws Exception {
+        MyLength length1 = new MyLength(100, "cm");
+        MyLength length2 = new MyLength(100, "cm");
+
+        MyLength result = length1.add(length2);
+        assertThat(result.getValue(), is(2.0));
+        assertThat(result.getUnit(), is("m"));
+    }
+
+    @Test
+    public void should_compare_two_length_with_same_unit() throws Exception {
+        MyLength length1 = new MyLength(10, "cm");
+        MyLength length2 = new MyLength(100, "cm");
+
+        int result = length1.compareTo(length2);
+
+        assertTrue(result < 0);
+    }
+
+    @Test
+    public void should_compare_two_length_with_different_unit() throws Exception {
+        MyLength length1 = new MyLength(100, "cm");
+        MyLength length2 = new MyLength(1, "m");
+
+        int result = length1.compareTo(length2);
+        assertEquals(result,0);
+    }
+
+    @Test
+    public void should_subtract_with_same_unit() throws Exception {
+        MyLength length1 = new MyLength(200, "m");
+        MyLength length2 = new MyLength(100, "m");
+        MyLength result = length1.subtract(length2);
+
+        assertThat(result.getValue(),is(100.0));
+        assertThat(result.getUnit(),is("m"));
+    }
+
+    @Test
+    public void should_subtract_with_different_unit() throws Exception {
+        MyLength length1 = new MyLength(2, "m");
+        MyLength length2 = new MyLength(100, "cm");
+
+        MyLength result = length1.subtract(length2);
+        assertThat(result.getValue(),is(1.0));
+        assertThat(result.getUnit(),is("m"));
+    }
+
+    @Test
+    public void should_multiply_with_same_unit() throws Exception {
+        MyLength length1 = new MyLength(100, "cm");
+        MyLength length2 = new MyLength(100, "cm");
+
+        double result = length1.multiply(length2);
+        assertThat(result,is(1.0));
+
+    }
+
+    @Test
+    public void should_multiply_with_different_unit() throws Exception {
+        MyLength length1 = new MyLength(100, "cm");
+        MyLength length2 = new MyLength(10, "m");
+
+        double result = length1.multiply(length2);
+
+        assertThat(result,is(10.0));
+    }
+
+    @Test
+    public void should_divide_with_same_unit() throws Exception {
+        MyLength length1 = new MyLength(100, "cm");
+        MyLength length2 = new MyLength(100, "cm");
+
+        double result = length1.divide(length2);
+
+        assertThat(result,is(1.0));
+    }
+
+    @Test
+    public void should_divide_with_different_unit() throws Exception {
+        MyLength length1 = new MyLength(10, "cm");
+        MyLength length2 = new MyLength(1, "m");
+        double divide = length1.divide(length2);
+        assertThat(divide,is(0.1));
+    }
+
+    @Test
+    public void should_convert_m_to_cm() throws Exception {
+        MyLength length1 = new MyLength(0.1, "m");
+
+        double cmValue = length1.convertToCm();
+
+        assertThat(cmValue,is(10.0));
+    }
+
+    @Test
+    public void should_convert_m_to_mm() throws Exception {
+        MyLength length2 = new MyLength(1, "m");
+        double mmValue = length2.convertToMm();
+
+        assertThat(mmValue,is(1000.0));
+    }
+
+    @Test
+    public void should_convert_cm_to_m() throws Exception {
+        MyLength length2 = new MyLength(100, "cm");
+        double mValue = length2.convertToM();
+
+        assertThat(mValue,is(1.0));
     }
 }
